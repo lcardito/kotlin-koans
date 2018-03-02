9a539f437f1bc58cd3ed268c9bcf81ccd4ce1025
luigicardito <lcardito@gmail.com>
2018-03-02 12:56:55 +0000

    Backup store

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,16 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+    override fun compareTo(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+    operator fun rangeTo(other: MyDate): DateRange = DateRange(this, other)
+
+    operator fun plus(interval: TimeInterval): MyDate = this.addTimeIntervals(interval, 1)
+}
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +18,20 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class RepeatedTimeInterval(val ti: TimeInterval, val n: Int = 1)
+
+class DateRange(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate>, Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> = DateRangeIterator(this)
+}
+
+class DateRangeIterator(private val dateRange: DateRange) : Iterator<MyDate> {
+    private var current = dateRange.start
+
+    override fun hasNext(): Boolean = current <= dateRange.endInclusive
+
+    override fun next(): MyDate {
+        val result = current
+        current = current.nextDay()
+        return result
+    }
+}
